d74c908148dd01d8530cbd0f1fbdd0da8c92b960
Prabha2110 <selvaprabhaselvam@gmail.com>
2025-04-15 12:20:12 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Prabha2110',
+    author_email= 'selvaprabhaselvam@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
